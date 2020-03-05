7a0c887522259ac70f2f9854979bd98a91ad5a92
ragtam <mag190033@gmail.com>
2020-03-05 10:00:29 +0100

    social sharing added

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 
+const SocialSharing = require('vue-social-sharing');
+
 Vue.config.productionTip = false;
+
+Vue.use(SocialSharing);
 
 new Vue({
     router,
