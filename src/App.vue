fd8dcb8fb940979aa8c022e78a7a58fc68c8754a
ragtam <mag190033@gmail.com>
2020-03-10 16:22:23 +0100

    Gapi setup leftovers

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
-  <div id="q-app">
-    <router-view />
-  </div>
+    <div id="q-app">
+        <router-view />
+    </div>
 </template>
 
 <script>
+import { SET_UP_GOOGLE_CLIENT_ID } from './store/mutation-types';
+import Vue from 'vue';
+
+const googleAuthenticationApi = 'https://apis.google.com/js/platform.js';
+
 export default {
-  name: 'App'
-}
+    name: 'App',
+    mounted() {
+        Vue.loadScript(googleAuthenticationApi).then(() => {
+            this.$store.dispatch(SET_UP_GOOGLE_CLIENT_ID);
+        });
+    },
+};
 </script>
