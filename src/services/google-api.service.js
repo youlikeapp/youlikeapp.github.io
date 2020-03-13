11c6bb54b596f3ebd793af8a55e5f8dfc7fb5bdb
ragtam <mag190033@gmail.com>
2020-03-13 16:12:39 +0100

    google sign in spec fixed

--- src/services/google-api.service.js
+++ src/services/google-api.service.js
@@ -1,11 +1,20 @@
 const clientConfiguration = {
-    clientId:
-        '830004684171-h17li43l6bp0j7nf1ln7slv3v6bdcvl0.apps.googleusercontent.com',
+    clientId: '830004684171-h17li43l6bp0j7nf1ln7slv3v6bdcvl0.apps.googleusercontent.com',
     scope: 'https://www.googleapis.com/auth/youtube',
 };
 const oauthClient = 'client:auth2';
 
 let authInstance;
+
+function setUpAuthInstance() {
+    return new Promise(resolve => {
+        gapi.load(oauthClient, () => {
+            gapi.client.init(clientConfiguration).then(() => {
+                authInstance = gapi.auth2.getAuthInstance();
+            });
+        });
+    });
+}
 
 function signIn() {
     return new Promise((resolve, reject) => {
@@ -36,29 +45,15 @@ function listenToSignedInChanges(callback) {
 }
 
 function getSignedInUser() {
-    const {
-        Ad: name,
-        jL: image,
-    } = authInstance.currentUser.get().getBasicProfile();
+    const { Ad: name, jL: image } = authInstance.currentUser.get().getBasicProfile();
     return { name, image };
 }
 
-function setUpAuthInstance() {
-    return new Promise(resolve => {
-        gapi.load(oauthClient, () => {
-            gapi.client.init(clientConfiguration).then(() => {
-                authInstance = gapi.auth2.getAuthInstance();
-                resolve(authInstance);
-            });
-        });
-    });
-}
-
 const googleApiService = {
+    setUpAuthInstance,
     signIn,
     logOff,
     listenToSignedInChanges,
-    setUpAuthInstance,
 };
 
 export default googleApiService;
