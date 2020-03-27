cd28526e383873eb3baf3bde4c9d639377a368db
ragtam <mag190033@gmail.com>
2020-03-27 19:48:19 +0100

    dialog done

--- src/components/VMainContentVideoListInput.vue
+++ src/components/VMainContentVideoListInput.vue
@@ -1,68 +1,82 @@
 <template>
     <div>
-        <h1>Восстановление лайков</h1>
         <div>
+            <h1>Восстановление лайков</h1>
             <div>
-                <!-- <md-content flex="70" flex-xs="100"> -->
-                <div>
-                    <div>
-                        <!-- <md-input-container class="md-block"> -->
-                        <label>Список видео (идентификаторы или полные ссылки)</label>
-                        <q-input v-model="videosString" filled autogrow />
-                        <!-- <textarea
-                                id="videosListTextArea"
-                                required
-                                rows="12"
-                                ng-class="$ctrl.videosListIsEmpty && 'list-empty'"
-                                ng-model="$ctrl.videosList"
-                                ng-focus="$ctrl.onListFocus()"
-                                ng-blur="$ctrl.onListBlur()"
-                                ng-change="$ctrl.onListChange()"
-                        ></textarea>-->
-                        <!-- </md-input-container> -->
-                    </div>
-                    <div>
-                        <div class="q-pa-md q-gutter-sm">
-                            <q-btn v-on:click="checkVideos()" v-bind:disabled="isListEmpty" icon="fas fa-tasks" label="Проверить" color="primary" />
-                            <q-btn v-on:click="saveList()" v-bind:disabled="isListEmpty" icon="fas fa-save" label="Сохранить" color="secondary" />
-                            <q-btn v-on:click="loadList()" v-if="!isListEmpty" icon="fas fa-file-download" label="Загрузить" color="red" />
-                            <q-btn v-on:click="deleteVideos()" v-if="!isListEmpty" icon="fas fa-trash-alt" label="Удалить" color="purple" />
-                        </div>
-                    </div>
-                    <!-- </md-content> -->
-                </div>
-                <div>
-                    <!-- <md-content ng-if="$ctrl.lastSuccessedVideos || $ctrl.lastFailedVideos"> -->
-                    <h3>Результат выставления лайков</h3>
-                    <p>Проставлено: $ctrl.lastSuccessedVideos.length</p>
-                    <p>Не проставлено: $ctrl.lastFailedVideos.length</p>
-                    <p>
-                        <strong>Лайки могут появиться с задержкой.</strong>
-                    </p>
-                    <div>
-                        ng-if="$ctrl.lastFailedVideos.length > 0"
-                        <button>
-                            ng-click="$ctrl.showDialog('failedVideosDialog')"
-                            <!-- <md-icon class="material-icons">&#xE417;</md-icon>Показать ошибки -->
-                            <span>Показать ошибки</span>
-                        </button>
-                    </div>
-                    <!-- </md-content> -->
-                </div>
-                <!-- <a id="{{ $ctrl.bottomId }}"></a> -->
-                a href with $ctrl.bottomId dynamic id
+                <label>Список видео (идентификаторы или полные ссылки)</label>
+                <q-input
+                    v-model="videosString"
+                    filled
+                    autogrow
+                    placeholder="placeholder to be filled in"
+                />
+            </div>
+            <div class="q-pa-md q-gutter-sm">
+                <q-btn
+                    v-on:click="checkVideos()"
+                    v-bind:disabled="isListEmpty"
+                    icon="fas fa-tasks"
+                    label="Проверить"
+                    color="primary"
+                />
+                <q-btn
+                    v-on:click="saveList()"
+                    v-bind:disabled="isListEmpty"
+                    icon="fas fa-save"
+                    label="Сохранить"
+                    color="secondary"
+                />
+                <q-btn
+                    v-on:click="loadList()"
+                    v-if="!isListEmpty"
+                    icon="fas fa-file-download"
+                    label="Загрузить"
+                    color="red"
+                />
+                <q-btn
+                    v-on:click="showDeleteConfirmationModal()"
+                    v-if="!isListEmpty"
+                    icon="fas fa-trash-alt"
+                    label="Удалить"
+                    color="purple"
+                />
             </div>
         </div>
-        <input id="forceUpdate" type="hidden" placeholder="on click $ctrl.forceUpdate()" />
+        <div>
+            <!-- <md-content ng-if="$ctrl.lastSuccessedVideos || $ctrl.lastFailedVideos"> -->
+            <h3>Результат выставления лайков</h3>
+            <p>Проставлено: $ctrl.lastSuccessedVideos.length</p>
+            <p>Не проставлено: $ctrl.lastFailedVideos.length</p>
+            <p>
+                <strong>Лайки могут появиться с задержкой.</strong>
+            </p>
+            <div>
+                ng-if="$ctrl.lastFailedVideos.length > 0"
+                <button>
+                    ng-click="$ctrl.showDialog('failedVideosDialog')"
+                    <!-- <md-icon class="material-icons">&#xE417;</md-icon>Показать ошибки -->
+                    <span>Показать ошибки</span>
+                </button>
+            </div>
+            <!-- </md-content> -->
+        </div>
+        <!-- <a id="{{ $ctrl.bottomId }}"></a> -->
+        a href with $ctrl.bottomId dynamic id
+        <input
+            id="forceUpdate"
+            type="hidden"
+            placeholder="on click $ctrl.forceUpdate()"
+        />
 
-        <hr />
-        <v-main-content-video-list-input-delete />
-        <hr />
+        <v-main-content-video-list-input-modal-delete-confirmation
+            v-bind:modal-visible="isDeleteModalVisible"
+            @close="deleteVideos"
+        />
     </div>
 </template>
 
 <script>
-import VMainContentVideoListInputDelete from './VMainContentVideoListInputDelete';
+import VMainContentVideoListInputModalDeleteConfirmation from './VMainContentVideoListInputModalDeleteConfirmation';
 
 export default {
     props: {
@@ -77,10 +91,10 @@ export default {
         },
     },
     data: function() {
-        return { videosString: '' };
+        return { videosString: '', isDeleteModalVisible: false };
     },
     components: {
-        VMainContentVideoListInputDelete,
+        VMainContentVideoListInputModalDeleteConfirmation,
     },
     methods: {
         checkVideos: function() {
@@ -92,8 +106,12 @@ export default {
         loadList: function() {
             this.$emit('load-list');
         },
-        deleteVideos: function() {
-            console.log('show confirmation dialog');
+        showDeleteConfirmationModal: function() {
+            this.isDeleteModalVisible = true;
+        },
+        deleteVideos: function(isOpened) {
+            this.isDeleteModalVisible = false;
+            console.log('deletion confirmed', isOpened);
         },
     },
     computed: {
---
+++ src/components/VMainContentVideoListInputModalDeleteConfirmation.vue
@@ -0,0 +1,45 @@
+<template>
+    <q-dialog v-model="visible" persistent>
+        <q-card>
+            <q-card-section class="row items-center">
+                <q-avatar icon="fas fa-trash-alt" color="primary" text-color="white" />
+                <span class="q-ml-sm">Вы уверены, что хотите удалить список видео?</span>
+            </q-card-section>
+
+            <q-card-actions align="right">
+                <q-btn flat label="Нет" color="primary" @click="accepted = false" v-close-popup />
+                <q-btn flat label="Да" color="primary" @click="accepted = true" v-close-popup />
+            </q-card-actions>
+        </q-card>
+    </q-dialog>
+</template>
+
+<script>
+export default {
+    props: {
+        modalVisible: Boolean,
+    },
+    data: function() {
+        return {
+            accepted: Boolean,
+        };
+    },
+    methods: {
+        close: function() {
+            this.$emit('close', { accepted: this.accepted });
+        },
+    },
+    computed: {
+        visible: {
+            get: function() {
+                return this.modalVisible;
+            },
+            set: function() {
+                this.close();
+            },
+        },
+    },
+};
+</script>
+
+<style></style>
